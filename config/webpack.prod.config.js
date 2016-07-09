eb70c9c97bfa7a7f8e50e26810b401d7e55c6152
jpuri <jyotipuri@gmail.com>
2016-07-09 21:04:04 +0530

    changes in webpack configurations

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,34 +1,34 @@
-const path = require('path');
-const webpack = require('webpack');
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   devtool: 'source-map',
   entry: [
-    './js/index',
+    './js/index'
   ],
   output: {
     path: path.join(__dirname, '../lib'),
     filename: 'index.js',
+    libraryTarget: 'commonjs2',
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
+        'NODE_ENV': JSON.stringify('production')
+      }
     }),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
-        warnings: false,
-      },
-    }),
+        warnings: false
+      }
+    })
   ],
   module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader' },
-    ],
-  },
-  resolve: {
-    extensions: ['', '.js', '.json'],
-  },
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, '../js')
+    }]
+  }
 };
