b31fd4dc81b3871940ccbdd306951772bb894ca1
jpuri <jyotipuri@gmail.com>
2017-04-09 12:02:10 +0530

    Fix for embedded links issue #12

--- js/block.js
+++ js/block.js
@@ -337,7 +337,7 @@ function getEntityMarkup(
     return `<img src="${entity.data.src}" style="float:${entity.data.alignment || 'none'};height: ${entity.data.height};width: ${entity.data.width}"/>`;
   }
   if (entity.type === 'EMBEDDED_LINK') {
-    return `<iframe width="${entity.data.width}" height="${entity.data.height}" src="${entity.data.src}" frameBorder="0" allowFullScreen />`;
+    return `<iframe width="${entity.data.width}" height="${entity.data.height}" src="${entity.data.src}" frameBorder="0" />`;
   }
   return text;
 }
