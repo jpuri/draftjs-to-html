247f268390acbf17db91c6dd44a6ec9478746e59
Duyle Campos <duyle@tangelo.ai>
2022-03-08 15:21:03 -0300

    fix: update nested list flow to be compliant with w3c standards

--- js/list.js
+++ js/list.js
@@ -1,22 +1,17 @@
-import {
-  getBlockTag,
-  getBlockStyle,
-  getBlockInnerMarkup,
-} from './block';
+import { getBlockTag, getBlockStyle, getBlockInnerMarkup } from './block';
 
 /**
-* Function to check if a block is of type list.
-*/
+ * Function to check if a block is of type list.
+ */
 export function isList(blockType) {
   return (
-    blockType === 'unordered-list-item'
-    || blockType === 'ordered-list-item'
+    blockType === 'unordered-list-item' || blockType === 'ordered-list-item'
   );
 }
 
 /**
-* Function will return html markup for a list block.
-*/
+ * Function will return html markup for a list block.
+ */
 export function getListMarkup(
   listBlocks,
   entityMap,
@@ -36,13 +31,17 @@ export function getListMarkup(
       listHtml.push(`<${getBlockTag(block.type)}>\n`);
     } else if (previousBlock.depth === block.depth) {
       if (nestedListBlock && nestedListBlock.length > 0) {
-        listHtml.push(getListMarkup(
-          nestedListBlock,
-          entityMap,
-          hashtagConfig,
-          directional,
-          customEntityTransform,
-        ));
+        listHtml.splice(
+          listHtml.length - 1,
+          0,
+          getListMarkup(
+            nestedListBlock,
+            entityMap,
+            hashtagConfig,
+            directional,
+            customEntityTransform,
+          ),
+        );
         nestedListBlock = [];
       }
     } else {
@@ -59,24 +58,30 @@ export function getListMarkup(
         listHtml.push(' dir = "auto"');
       }
       listHtml.push('>');
-      listHtml.push(getBlockInnerMarkup(
-        block,
-        entityMap,
-        hashtagConfig,
-        customEntityTransform,
-      ));
+      listHtml.push(
+        getBlockInnerMarkup(
+          block,
+          entityMap,
+          hashtagConfig,
+          customEntityTransform,
+        ),
+      );
       listHtml.push('</li>\n');
       previousBlock = block;
     }
   });
   if (nestedListBlock && nestedListBlock.length > 0) {
-    listHtml.push(getListMarkup(
-      nestedListBlock,
-      entityMap,
-      hashtagConfig,
-      directional,
-      customEntityTransform,
-    ));
+    listHtml.splice(
+      listHtml.length - 1,
+      0,
+      getListMarkup(
+        nestedListBlock,
+        entityMap,
+        hashtagConfig,
+        directional,
+        customEntityTransform,
+      ),
+    );
   }
   listHtml.push(`</${getBlockTag(previousBlock.type)}>\n`);
   return listHtml.join('');
